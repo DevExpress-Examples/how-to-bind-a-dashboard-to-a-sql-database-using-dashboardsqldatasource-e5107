4b58caad336518b0bede1bba05c80737c2e3bd7a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_DashboardDataProviders/Form1.cs
@@ -0,0 +1,51 @@
+﻿using DevExpress.XtraBars.Ribbon;
+using DevExpress.DashboardCommon;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
+
+namespace Dashboard_SqlDataProvider {
+    public partial class Form1 : RibbonForm {
+        public Form1() {
+            InitializeComponent();
+            dashboardDesigner1.CreateRibbon();
+
+            #region #SQLDataSource
+            Access97ConnectionParameters access97Params = new Access97ConnectionParameters();
+            access97Params.FileName = @"..\..\Data\nwind.mdb";
+
+            DashboardSqlDataSource sqlDataSource = 
+                new DashboardSqlDataSource("SQL Data Source 1", access97Params);
+            TableQuery tableQuery = new TableQuery("Query 1");
+            tableQuery.AddTable("SalesPerson").SelectColumns("CategoryName", 
+                                                             "Sales Person", 
+                                                             "OrderDate", 
+                                                             "Extended Price");
+            sqlDataSource.Queries.Add(tableQuery);
+            sqlDataSource.Fill();
+
+            dashboardDesigner1.Dashboard.DataSources.Add(sqlDataSource);
+            #endregion #SQLDataSource
+
+            InitializeDashboardItems();
+        }
+
+        void InitializeDashboardItems() {
+            IDashboardDataSource sqlDataSource = dashboardDesigner1.Dashboard.DataSources[0];
+
+            ChartDashboardItem chart = new ChartDashboardItem();
+            chart.DataSource = sqlDataSource; chart.DataMember = "Query 1";
+            chart.Arguments.Add(new Dimension("OrderDate", DateTimeGroupInterval.MonthYear));
+            chart.Panes.Add(new ChartPane());
+            SimpleSeries salesAmountSeries = new SimpleSeries(SimpleSeriesType.SplineArea);
+            salesAmountSeries.Value = new Measure("Extended Price");
+            chart.Panes[0].Series.Add(salesAmountSeries);
+
+            GridDashboardItem grid = new GridDashboardItem();
+            grid.DataSource = sqlDataSource; grid.DataMember = "Query 1";
+            grid.Columns.Add(new GridDimensionColumn(new Dimension("Sales Person")));
+            grid.Columns.Add(new GridMeasureColumn(new Measure("Extended Price")));
+
+            dashboardDesigner1.Dashboard.Items.AddRange(chart, grid);
+        }
+    }
+}
---
+++ VB/Dashboard_DashboardDataProviders/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports DevExpress.XtraBars.Ribbon
+Imports DevExpress.DashboardCommon
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess.Sql
+
+Namespace Dashboard_SqlDataProvider
+    Partial Public Class Form1
+        Inherits RibbonForm
+
+        Public Sub New()
+            InitializeComponent()
+            dashboardDesigner1.CreateRibbon()
+
+'            #Region "#SQLDataSource"
+            Dim access97Params As New Access97ConnectionParameters()
+            access97Params.FileName = "..\..\Data\nwind.mdb"
+
+            Dim sqlDataSource As New DashboardSqlDataSource("SQL Data Source 1", access97Params)
+            Dim tableQuery As New TableQuery("Query 1")
+            tableQuery.AddTable("SalesPerson").SelectColumns("CategoryName", _
+                                                             "Sales Person", _
+                                                             "OrderDate", _
+                                                             "Extended Price")
+            sqlDataSource.Queries.Add(tableQuery)
+            sqlDataSource.Fill()
+
+            dashboardDesigner1.Dashboard.DataSources.Add(sqlDataSource)
+'            #End Region ' #SQLDataSource
+
+            InitializeDashboardItems()
+        End Sub
+
+        Private Sub InitializeDashboardItems()
+            Dim sqlDataSource As IDashboardDataSource = dashboardDesigner1.Dashboard.DataSources(0)
+
+            Dim chart As New ChartDashboardItem()
+            chart.DataSource = sqlDataSource
+            chart.DataMember = "Query 1"
+            chart.Arguments.Add(New Dimension("OrderDate", DateTimeGroupInterval.MonthYear))
+            chart.Panes.Add(New ChartPane())
+            Dim salesAmountSeries As New SimpleSeries(SimpleSeriesType.SplineArea)
+            salesAmountSeries.Value = New Measure("Extended Price")
+            chart.Panes(0).Series.Add(salesAmountSeries)
+
+            Dim grid As New GridDashboardItem()
+            grid.DataSource = sqlDataSource
+            grid.DataMember = "Query 1"
+            grid.Columns.Add(New GridDimensionColumn(New Dimension("Sales Person")))
+            grid.Columns.Add(New GridMeasureColumn(New Measure("Extended Price")))
+
+            dashboardDesigner1.Dashboard.Items.AddRange(chart, grid)
+        End Sub
+    End Class
+End Namespace
